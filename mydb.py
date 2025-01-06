798abd17de015367a879cbbc3bf73c5522ed12f1
Abin Roy <abinroy510@gmail.com>
2025-01-06 17:31:02 +0600

    initial commit

---
+++ mydb.py
@@ -0,0 +1,19 @@
+#install mysql on local machine
+#pip install mysql
+#pip install mysql-connector
+#pip install mysql-connector-python
+
+
+import mysql.connector
+dataBase = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='abinroy'
+    
+)
+#cursor object
+cursorObject =dataBase.cursor()
+
+#database
+cursorObject.execute("CREATE DATABASE abinco")
+print("All Done!")
